8d6a95494bb9b0173f4be5cdff9042846c56d796
AveragelyIntelligentBird <anfisabogdanenko@gmail.com>
2024-06-18 21:04:11 -0400

    Completes A6 + fixes kanbas db login

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import UserRoutes from "./Users/routes.js";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING.concat('/kanbas') || "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING)
     .then(r => console.log("Connected to MongoDB at " + CONNECTION_STRING))
     .catch(e => console.log(e));
